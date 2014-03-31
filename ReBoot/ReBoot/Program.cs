bf4b212cbc6b89bd6d4beaddeb25d35fa72d0c78
qv15mail <qv15@qq.com>
2014-03-31 14:15:38 +0800

    wince下的重启和删掉进程

---
+++ ReBoot/ReBoot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReBoot
+{
+    class Program
+    {
+        const int POWER_STATE_RESET = 0x800000;
+        [System.Runtime.InteropServices.DllImport("coredll.dll")]
+        private static extern Int32 SetSystemPowerState(System.Char[] psState, System.Int32 StateFlags, System.Int32 Options);
+        public static void WarmReset()
+        {
+            SetSystemPowerState(null, POWER_STATE_RESET, 0);
+        }
+
+        static void Main(string[] args)
+        {
+            WarmReset();
+        }
+    }
+}
---
+++ killtask/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace killtask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+            //Console.WriteLine(path);
+            ProcessCE.FindAndKill(path + "NAND\\smartsign.exe");
+            ProcessCE.FindAndKill(path + "NAND\\launch.exe");
+        }
+    }
+}
